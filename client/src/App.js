976c9b1019c77ae5e907bc3936bfc38920cf8ef5
KenZync <kenphati@hotmail.com>
2021-11-13 18:18:14 +0700

    Mockup Fund Ranking Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,312 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import {
+  Camera,
+  FirstPage,
+  FormatAlignCenter,
+  FormatAlignLeft,
+  FormatAlignRight,
+  KeyboardArrowLeft,
+  KeyboardArrowRight,
+  Laptop,
+  LastPage,
+  PhoneAndroid,
+  Tv,
+} from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  createTheme,
+  CssBaseline,
+  Grid,
+  IconButton,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  ThemeProvider,
+  ToggleButton,
+  ToggleButtonGroup,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useTheme } from "@mui/system";
+import { DataGrid } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import "./App.css";
+
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPage /> : <FirstPage />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPage /> : <LastPage />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+function createData(name, calories, fat) {
+  return { name, calories, fat };
+}
+
+const rows = [
+  createData("Cupcake", 305, 3.7),
+  createData("Donut", 452, 25.0),
+  createData("Eclair", 262, 16.0),
+  createData("Frozen yoghurt", 159, 6.0),
+  createData("Gingerbread", 356, 16.0),
+  createData("Honeycomb", 408, 3.2),
+  createData("Ice cream sandwich", 237, 9.0),
+  createData("Jelly Bean", 375, 0.0),
+  createData("KitKat", 518, 26.0),
+  createData("Lollipop", 392, 0.2),
+  createData("Marshmallow", 318, 0),
+  createData("Nougat", 360, 19.0),
+  createData("Oreo", 437, 18.0),
+].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+
 function App() {
+  const theme = createTheme({
+    palette: {
+      background: {
+        default: "#F3F4F5",
+      },
+      primary: {
+        main: "#FACD00",
+      },
+      // secondary: {
+      //   main: "#1D1D1D",
+      // },
+    },
+  });
+
+  const [timeRange, setTimeRange] = useState("1D");
+
+  const handleAlignment = (event, newAlignment) => {
+    if (newAlignment !== null) {
+      setTimeRange(newAlignment);
+    }
+  };
 
   const [data, setData] = useState(null);
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Camera sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            FINNOMENA
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 4 }}>
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Fund Ranking
+          </Typography>
+        </Container>
+        <Container component="main">
+          Time Length
+          <Paper sx={{ mb: 2, mt: 2 }}>
+            <Stack direction="row" spacing={4}>
+              <ToggleButtonGroup
+                value={timeRange}
+                exclusive
+                onChange={handleAlignment}
+                aria-label="text alignment"
+                fullWidth
+              >
+                <ToggleButton
+                  color="primary"
+                  value="1D"
+                  aria-label="left aligned"
+                >
+                  1 Day
+                </ToggleButton>
+                <ToggleButton color="primary" value="1W" aria-label="centered">
+                  1 Week
+                </ToggleButton>
+                <ToggleButton color="primary" value="1M" aria-label="centered">
+                  1 Month
+                </ToggleButton>
+                <ToggleButton
+                  color="primary"
+                  value="1Y"
+                  aria-label="right aligned"
+                >
+                  1 Year
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Stack>
+          </Paper>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="right">Rank of fund</TableCell>
+                  <TableCell align="right">Updated Date</TableCell>
+                  <TableCell align="right">Performance</TableCell>
+                  <TableCell align="right">Price</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {(rowsPerPage > 0
+                  ? rows.slice(
+                      page * rowsPerPage,
+                      page * rowsPerPage + rowsPerPage
+                    )
+                  : rows
+                ).map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell style={{ width: 160 }} align="right">
+                      {row.calories}
+                    </TableCell>
+                    <TableCell style={{ width: 160 }} align="right">
+                      {row.fat}
+                    </TableCell>
+                  </TableRow>
+                ))}
+
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 53 * emptyRows }}>
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    rowsPerPageOptions={[
+                      5,
+                      10,
+                      25,
+                      { label: "All", value: -1 },
+                    ]}
+                    colSpan={3}
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    SelectProps={{
+                      inputProps: {
+                        "aria-label": "rows per page",
+                      },
+                    }}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                    ActionsComponent={TablePaginationActions}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </TableContainer>
+        </Container>
+      </main>
+      {/* Footer */}
+    </ThemeProvider>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const express = require('express'); //Line 1
-const path = require('path');
+const express = require("express"); //Line 1
+const path = require("path");
 const app = express(); //Line 2
 const port = process.env.PORT || 3001; //Line 3
+const axios = require("axios");
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
@@ -15,6 +16,6 @@ app.get("/api", (req, res) => {
 });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });

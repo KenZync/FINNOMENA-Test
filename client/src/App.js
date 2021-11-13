d6b9a22a9f74a7536705f41dde3152bc1e39b09c
KenZync <kenphati@hotmail.com>
2021-11-13 19:02:13 +0700

    Move TablePaginationActions to Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,9 @@
-import {
-  Camera,
-  FirstPage,
-  KeyboardArrowLeft,
-  KeyboardArrowRight,
-  LastPage,
-} from "@mui/icons-material";
+import { Camera } from "@mui/icons-material";
 import {
   AppBar,
-  Box,
   Container,
   createTheme,
   CssBaseline,
-  IconButton,
   Paper,
   Stack,
   Table,
@@ -28,81 +20,10 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import { useTheme } from "@mui/system";
-import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
-import PropTypes from "prop-types";
 import "./App.css";
 import axios from "axios";
-
-function TablePaginationActions(props) {
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onPageChange } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === "rtl" ? <LastPage /> : <FirstPage />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === "rtl" ? <FirstPage /> : <LastPage />}
-      </IconButton>
-    </Box>
-  );
-}
-
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onPageChange: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
+import TablePaginationActions from "./Components/TablePaginationActions";
 
 function App() {
   const theme = createTheme({
@@ -145,11 +66,7 @@ function App() {
     setPage(0);
   };
 
-  // useEffect(() => {
-  //   fetch("/api")
-  //     .then((res) => res.json())
-  //     .then((data) => setData(data.message));
-  // }, []);
+  
 
   useEffect(() => {
     axios
@@ -187,7 +104,7 @@ function App() {
           </Typography>
         </Container>
         <Container component="main">
-          Time Length
+          Select Time Range
           <Paper sx={{ mb: 2, mt: 2 }}>
             <Stack direction="row" spacing={4}>
               <ToggleButtonGroup
@@ -247,18 +164,10 @@ function App() {
                       <TableCell component="th" scope="row">
                         {row.thailand_fund_code}
                       </TableCell>
-                      <TableCell style={{ width: 160 }} align="right">
-                        {row.nav_return}
-                      </TableCell>
-                      <TableCell style={{ width: 160 }} align="right">
-                        {row.nav_date}
-                      </TableCell>
-                      <TableCell style={{ width: 160 }} align="right">
-                        {row.nav_return}
-                      </TableCell>
-                      <TableCell style={{ width: 160 }} align="right">
-                        {row.nav}
-                      </TableCell>
+                      <TableCell align="right">{row.nav_return}</TableCell>
+                      <TableCell align="right">{row.nav_date}</TableCell>
+                      <TableCell align="right">{row.nav_return}</TableCell>
+                      <TableCell align="right">{row.nav}</TableCell>
                     </TableRow>
                   ))}
 
